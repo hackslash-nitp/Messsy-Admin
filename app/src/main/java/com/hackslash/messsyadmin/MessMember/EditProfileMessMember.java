20e8acc2f71c8d7ed814c9f86a96747fcfe767a1
Tanvi Tanya <tanvitanya15@gmail.com>
2021-06-10 14:43:34 +0530

    Edit Profile Mess Member Activity

---
+++ app/src/main/java/com/hackslash/messsyadmin/MessMember/EditProfileMessMember.java
@@ -0,0 +1,16 @@
+package com.hackslash.messsyadmin.MessMember;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hackslash.messsyadmin.R;
+
+public class EditProfileMessMember extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile_mess_member);
+    }
+}
