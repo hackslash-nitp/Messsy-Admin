efa2fbef72b99ac463563941e580f91b20a8f0eb
Tanvi Tanya <85440564+tanvitanyaa@users.noreply.github.com>
2021-08-20 22:03:54 +0530

    Setup Report Issue Activity (#51)
     Report Issue data stored on firestore database

--- app/src/main/java/com/hackslash/messsyadmin/Activity/MessReportIssueActivity.java
+++ app/src/main/java/com/hackslash/messsyadmin/Activity/MessReportIssueActivity.java
@@ -1,19 +1,30 @@
 package com.hackslash.messsyadmin.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.hackslash.messsyadmin.Model.ReportIssue;
 import com.hackslash.messsyadmin.R;
 
 import java.io.FileNotFoundException;
@@ -25,12 +36,25 @@ public class MessReportIssueActivity extends AppCompatActivity {
     EditText issueET, explainationET;
     TextView informationTV;
     String sIssue , sExplaination ;
+    ImageView sImage;
+
+    Dialog dialogIssueReported;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser currentUser;
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+
     private static int PICK_IMAGE = 1 ;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mess_reportissue);
         Intent intentReport = getIntent();
+
 
         backButton = (Button) findViewById(R.id.backButton_report);
         sendButton = (Button) findViewById(R.id.sendButton);
@@ -39,41 +63,85 @@ public class MessReportIssueActivity extends AppCompatActivity {
         explainationET = (EditText) findViewById(R.id.explaination);
         informationTV = (TextView) findViewById(R.id.information);
 
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
                 sIssue = issueET.getText().toString();
                 sExplaination = explainationET.getText().toString();
-                Toast.makeText(MessReportIssueActivity.this, "Issue:" + sIssue + "\nExplaination:"+ sExplaination, Toast.LENGTH_SHORT).show();
+
+                if(sIssue.isEmpty())
+                {
+                    issueET.setError("Subject Required!");
+                    return;
+                }
+
+                if(sExplaination.isEmpty())
+                {
+                    explainationET.setError("Please describe your issue.");
+                    return;
+                }
+
+                ReportIssue ReportInfo = new ReportIssue(sIssue, sExplaination, sImage);
+
+                currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+                firebaseFirestore.collection("Issues").document(currentUser.getUid()).set(ReportInfo).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                        OpenDialog();
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        Toast.makeText(MessReportIssueActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
             }
         });
+
+
 
         uploadImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), PICK_IMAGE);
+
             }
+
         });
 
         informationTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Toast.makeText(MessReportIssueActivity.this, "Clicked on Data Use Policy", Toast.LENGTH_SHORT).show();
+
             }
         });
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 finish();
+
             }
         });
 
-
-
-
-
     }
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -85,6 +153,7 @@ public class MessReportIssueActivity extends AppCompatActivity {
             Bitmap bitmap = null;
             try {
                 bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                sImage.setImageBitmap(bitmap);
             } catch (FileNotFoundException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
@@ -92,9 +161,33 @@ public class MessReportIssueActivity extends AppCompatActivity {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
+
         }
 
         uploadImageButton.setText("Image Uploaded");
         uploadImageButton.setBackgroundResource(R.drawable.uploadimageafteruploaded);
     }
+
+    public void OpenDialog(){
+        dialogIssueReported.setContentView(R.layout.report_sent_dialog);
+        dialogIssueReported.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialogIssueReported.setCancelable(false);
+        dialogIssueReported.setCanceledOnTouchOutside(false);
+
+        Button btnDone = dialogIssueReported.findViewById(R.id.btn_Done);
+
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                dialogIssueReported.dismiss();
+                Intent sendToMessFragmentContainerIntent = new Intent(getApplicationContext(), MessFragmentContainer.class);
+                startActivity(sendToMessFragmentContainerIntent);
+            }
+        });
+
+        dialogIssueReported.show();
+
+    }
+
 }
